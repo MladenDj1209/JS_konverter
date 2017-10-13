3f361e646c8b09990d6c359a11a17c3cf28df368
Mladen <djomla.dj@gmail.com>
2017-10-13 10:37:08 +0200

    koverter update

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 let eur;
 let usd;
+let chf;
 
 const konvertor = function(bam) {
 	 eur = bam/1.95;
 	 usd = bam*0.61;
-
+	 chf = bam*0.6;
 	return {
-		eur, usd
+		eur, usd, chf
 	}
 }
 
-konvertor(10);
-console.log('Iznos u eurima je:' +eur);
-console.log('Iznos u dolarima je:' +usd);
+konvertor(15);
+console.log('Iznos u EUR je: ' +eur);
+console.log('Iznos u USD je: ' +usd);
+console.log('Iznos u CHF je: ' +chf);
