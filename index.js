6e80b9d59ed8492935f896d5c8895ed68e178e84
Mladen <djomla.dj@gmail.com>
2017-10-13 10:39:38 +0200

    konverter update

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let eur;
 let usd;
 let chf;
+let htk;
 
 const konvertor = function(bam) {
 	 eur = bam/1.95;
 	 usd = bam*0.61;
 	 chf = bam*0.6;
+	 hrk = bam*3.84;
 	return {
 		eur, usd, chf
 	}
@@ -15,3 +17,4 @@ konvertor(15);
 console.log('Iznos u EUR je: ' +eur);
 console.log('Iznos u USD je: ' +usd);
 console.log('Iznos u CHF je: ' +chf);
+console.log('Iznos u HRK je: ' +hrk);
